82d33161d2b89270da8e855aef2734cf4cb7d728
TDC-Cyrille <47850156+TDC-Cyrille@users.noreply.github.com>
2021-04-03 03:03:25 +0800

    Initial update ics.php
    This update:
     - Fixes the event not being created in Thunderbird - and possibly other email clients - by using PHP_EOL instead of /r/n
     - Adds a proper timezone support (note that the timezone is Asia/Singapore, you must edit ics.php to change it, we may release a new update in the future to correct this)
     - Adds an alarm for 15 minutes before the event
     - Fixes some issue with the description escaping that was broken if HTML was input

--- ICS.php
+++ ICS.php
@@ -40,92 +40,153 @@
  *   or https://).
  */
 
-class ICS {
-  const DT_FORMAT = 'Ymd\THis\Z';
+	class ICS {
 
-  protected $properties = array();
-  private $available_properties = array(
-    'description',
-    'dtend',
-    'dtstart',
-    'location',
-    'summary',
-    'url'
-  );
+		const DT_FORMAT = 'Ymd\THis';
 
-  public function __construct($props) {
-    $this->set($props);
-  }
+		protected $properties = array();
+		private $available_properties = array(
+			'description',
+			'dtend',
+			'dtstart',
+			'location',
+			'summary',
+			'url'
+		);
 
-  public function set($key, $val = false) {
-    if (is_array($key)) {
-      foreach ($key as $k => $v) {
-        $this->set($k, $v);
-      }
-    } else {
-      if (in_array($key, $this->available_properties)) {
-        $this->properties[$key] = $this->sanitize_val($val, $key);
-      }
-    }
-  }
+		public function __construct( $props ) {
+			
+			$this->set( $props );
+			
+		}
 
-  public function to_string() {
-    $rows = $this->build_props();
-    return implode("\r\n", $rows);
-  }
+		public function set( $key, $val = false ) {
+			
+			if( is_array( $key ) )
+				foreach( $key as $k => $v )
+					$this->set( $k, $v );
+			else
+				if( in_array( $key, $this->available_properties ) )
+					$this->properties[$key] = $this->sanitize_val( $val, $key );
+			
+		}
 
-  private function build_props() {
-    // Build ICS properties - add header
-    $ics_props = array(
-      'BEGIN:VCALENDAR',
-      'VERSION:2.0',
-      'PRODID:-//hacksw/handcal//NONSGML v1.0//EN',
-      'CALSCALE:GREGORIAN',
-      'BEGIN:VEVENT'
-    );
+		public function to_string() {
+			
+			$rows = $this->build_props();
+			
+			return implode( PHP_EOL, $rows );
+		
+		}
 
-    // Build ICS properties - add header
-    $props = array();
-    foreach($this->properties as $k => $v) {
-      $props[strtoupper($k . ($k === 'url' ? ';VALUE=URI' : ''))] = $v;
-    }
+		private function build_props() {
 
-    // Set some default values
-    $props['DTSTAMP'] = $this->format_timestamp('now');
-    $props['UID'] = uniqid();
+			$ics_props = array(
+				'BEGIN:VCALENDAR',
+				'VERSION:2.0',
+				'PRODID:-//hacksw/handcal//NONSGML v1.0//EN',
+				'CALSCALE:GREGORIAN',
+				'BEGIN:VTIMEZONE',
+				'TZID:Asia/Singapore',
+				'TZURL:https://tzurl.org/zoneinfo-outlook/Asia/Singapore',
+				'X-LIC-LOCATION:Asia/Singapore',
+				'BEGIN:STANDARD',
+				'TZOFFSETFROM:+0800',
+				'TZOFFSETTO:+0800',
+				'TZNAME:CST',
+				'DTSTART:19700101T000000',
+				'END:STANDARD',
+				'END:VTIMEZONE',
+				'BEGIN:VEVENT'
+			);
 
-    // Append properties
-    foreach ($props as $k => $v) {
-      $ics_props[] = "$k:$v";
-    }
+			$props = array();
+			foreach( $this->properties as $k => $v ) {
+				
+				switch( $k ) {
+					
+					case 'url':
+					
+						$props[strtoupper( $k.';VALUE=URI' )] = $v;
+						
+						break;
+					
+					case 'dtstart':
+					case 'dtend':
+					
+						$props[strtoupper( $k ).';TZID=Asia/Singapore'] = $v;
+						
+						break;
+					
+					default:
+					
+						$props[strtoupper( $k )] = $v;
+					
+						break;
+					
+				}
+				
+			}
 
-    // Build ICS properties - add footer
-    $ics_props[] = 'END:VEVENT';
-    $ics_props[] = 'END:VCALENDAR';
+			$props['DTSTAMP'] = $this->format_timestamp( 'now' );
+			$props['UID'] = uniqid();
 
-    return $ics_props;
-  }
+			foreach( $props as $k => $v )
+				$ics_props[] = "$k:$v";
+				
+			$ics_props[] = 'BEGIN:VALARM';
+			$ics_props[] = 'ACTION:DISPLAY';
+			$ics_props[] = 'DESCRIPTION:'.$this->properties['summary'];
+			$ics_props[] = 'TRIGGER:-PT15M';
+			$ics_props[] = 'END:VALARM';
+			$ics_props[] = 'END:VEVENT';
+			$ics_props[] = 'END:VCALENDAR';
 
-  private function sanitize_val($val, $key = false) {
-    switch($key) {
-      case 'dtend':
-      case 'dtstamp':
-      case 'dtstart':
-        $val = $this->format_timestamp($val);
-        break;
-      default:
-        $val = $this->escape_string($val);
-    }
+			return $ics_props;
+			
+		}
 
-    return $val;
-  }
+		private function sanitize_val( $val, $key = false ) {
+			
+			switch( $key ) {
+				
+				case 'dtend':
+				case 'dtstamp':
+				case 'dtstart':
+				
+					$val = $this->format_timestamp($val);
+					
+					break;
+					
+				default:
+				
+					$val = $this->escape_string($val);
+					
+					break;
+				
+			}
 
-  private function format_timestamp($timestamp) {
-    $dt = new DateTime($timestamp);
-    return $dt->format(self::DT_FORMAT);
-  }
+			return $val;
+			
+		}
 
-  private function escape_string($str) {
-    return preg_replace('/([\,;])/','\\\$1', $str);
-  }
-}
+		private function format_timestamp( $timestamp ) {
+
+			$dt = new DateTime( $timestamp );
+
+			return $dt->format( self::DT_FORMAT );
+
+		}
+
+		private function escape_string( $str ) {
+
+			$str = str_replace( '&nbsp;', ' ', $str );
+			$str = str_replace( array( '<br>', '<br />', '<br/>' ), '\n', $str );
+			
+			return preg_replace( '/([\,;])/','\\\$1', $str );
+
+		}
+	  
+	}
+
+?>
